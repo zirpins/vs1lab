030a58c2d4056a9637513716f71465809c137eee
Jonas Westenhoff <jwestenhoff@web.de>
2020-06-25 17:53:53 +0200

    add files from previous tasks

---
+++ Aufgabe4/gta_v4/gta-server.js
@@ -0,0 +1,258 @@
+/**
+ * Template für Übungsaufgabe VS1lab/Aufgabe3
+ * Das Skript soll die Serverseite der gegebenen Client Komponenten im
+ * Verzeichnisbaum implementieren. Dazu müssen die TODOs erledigt werden.
+ */
+
+/**
+ * Definiere Modul Abhängigkeiten und erzeuge Express app.
+ */
+
+var http = require('http');
+//var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var app;
+app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Setze ejs als View Engine
+app.set('view engine', 'ejs');
+
+/**
+ * Konfiguriere den Pfad für statische Dateien.
+ * Teste das Ergebnis im Browser unter 'http://localhost:3000/'.
+ */
+
+app.use(express.static(__dirname + '/public'));
+
+/**
+ * Konstruktor für GeoTag Objekte.
+ * GeoTag Objekte sollen min. alle Felder des 'tag-form' Formulars aufnehmen.
+ */
+
+function GeoTag(latitude, longitude, name, hashtag) {
+    this.latitude = latitude
+    this.longitude = longitude
+    this.name = name
+    this.hashtag = hashtag
+
+    this.getName = function () {
+        return this.name;
+    };
+
+    this.getLatitude = function () {
+        return this.latitude;
+    };
+
+    this.getLongitude = function () {
+        return this.longitude;
+    };
+
+    this.getHashtag = function () {
+        return this.hashtag;
+    };
+
+    this.toString = function () {
+        return "GeoTag: [name=" + this.name + "][latitude=" + this.latitude + "][longitude=" + this.longitude + "][hashtag=" + this.hashtag + "]";
+    }
+};
+
+/**
+ * Modul für 'In-Memory'-Speicherung von GeoTags mit folgenden Komponenten:
+ * - Array als Speicher für Geo Tags.
+ * - Funktion zur Suche von Geo Tags in einem Radius um eine Koordinate.
+ * - Funktion zur Suche von Geo Tags nach Suchbegriff.
+ * - Funktion zum hinzufügen eines Geo Tags.
+ * - Funktion zum Löschen eines Geo Tags.
+ */
+
+var geoTagModul = (function() {
+    // Private
+    var tagList = [];
+    /**
+     * Converts degree to radian values
+     * @param {double} degree Degree to be converted to radian
+     */
+    var degreeToRadian = function (degree) {
+        return degree * (Math.PI/180)
+    }
+    // Public
+    return {
+        /**
+         * Returns an array of GeoTags in a certain radius
+         * @param {double} latitude Latitude in °
+         * @param {double} longitude Longitude in °
+         * @param {double} radius Radius in km
+         * @param {GeoTag} list (Optional) Array of GeoTags
+         */
+        searchByRadius: function(latitude, longitude, radius, list) {
+            if (list === undefined) {
+                list = tagList
+            }
+            var results = []
+            // Comparing every value with the Haversine formula for distance
+            list.forEach(function (geoTag) {
+                var latitude1 = degreeToRadian(latitude)
+                var latitude2= degreeToRadian(geoTag.latitude)
+                var divLat = degreeToRadian(geoTag.latitude - latitude)
+                var divLon = degreeToRadian(geoTag.longitude - longitude)
+                var distance = Math.sin(divLat/2) * Math.sin(divLat/2) 
+                    + Math.cos(latitude1) * Math.cos(latitude2)
+                    * Math.sin(divLon/2) * Math.sin(divLon/2);
+                distance = 2 * Math.atan2(Math.sqrt(distance), Math.sqrt(1-distance))
+                distance = distance * 6371
+                console.log(distance)
+                if (distance <= radius) {
+                    results.push(geoTag)
+                }
+            });
+            return results
+        },
+
+        /**
+         * Returns an array of Geotags that have the searchTerm as an infix in their name or hashtag
+         * @param {String} searchTerm Infix to look for
+         * @param {GeoTag} list (Optional) Array of GeoTags
+         */
+        searchByTerm: function(searchTerm, list) {
+            if (list === undefined) {
+                list = tagList
+            }
+            var results = []
+            var reg = new RegExp(searchTerm, 'i')
+            results = list.filter(function (geoTag) {
+                return (reg.test(geoTag.name) || reg.test(geoTag.hashtag))
+            })
+            return results
+        },
+
+        /**
+         * Adds a GeoTag to the list
+         * @param {latitude} latitude of new geo tag
+         * @param {longitude} longitude of new geo tag
+         * @param {name} name of new geo tag
+         * @param {hashtag} hashtag of new geo tag
+         */
+        addGeoTag: function (latitude, longitude, name, hashtag) {
+            tagList.push(new GeoTag(latitude, longitude, name, hashtag));
+        },
+
+        /**
+         * Deletes a GeoTag
+         * @param {int} index Index of the GeoTag to be deleted
+         */
+        deleteGeoTagByIndex: function(index) {
+            tagList.splice(index)
+        },
+
+        /**
+         * Deletes geo tag
+         * @param {geoTag} geo tag to delete
+         */
+        deleteGeoTagByGeoTag: function (geoTag) {
+            if (geoTag instanceof GeoTag) {
+                for (var i = 0; i < tagList.length; i++) {
+                    if (tagList[i].name === geoTag.name
+                        && tagList[i].latitude === geoTag.latitude
+                        && tagList[i].longitude === geoTag.longitue
+                        && tagList[i].hashtag === geoTag.hashtag) {
+
+                        tagList.slice(i + 1);
+                    }
+                }
+            }
+        },
+    };
+})();
+
+/**
+ * Route mit Pfad '/' für HTTP 'GET' Requests.
+ * (http://expressjs.com/de/4x/api.html#app.get.method)
+ *
+ * Requests enthalten keine Parameter
+ *
+ * Als Response wird das ejs-Template ohne Geo Tag Objekte gerendert.
+ */
+
+app.get('/', function(req, res) {
+    res.render('gta', {
+        taglist: [],
+        lati: req.body.latitude,
+        longi: req.body.longitude
+    });
+});
+
+/**
+ * Route mit Pfad '/tagging' für HTTP 'POST' Requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
+ *
+ * Requests enthalten im Body die Felder des 'tag-form' Formulars.
+ * (http://expressjs.com/de/4x/api.html#req.body)
+ *
+ * Mit den Formulardaten wird ein neuer Geo Tag erstellt und gespeichert.
+ *
+ * Als Response wird das ejs-Template mit Geo Tag Objekten gerendert.
+ * Die Objekte liegen in einem Standard Radius um die Koordinate (lat, lon).
+ */
+
+app.post('/tagging', function (req, res) {
+    // Create new GeoTag
+    geoTagModul.addGeoTag(req.body.latitude, req.body.longitude, req.body.name, req.body.hashtag)
+    // Create list of GeoTags in a certain radius
+    var toRender = geoTagModul.searchByRadius(req.body.latitude, req.body.longitude, 5)
+    res.render('gta', {
+        taglist: toRender,
+        lati: req.body.latitude,
+        longi: req.body.longitude
+    });
+});
+
+/**
+ * Route mit Pfad '/discovery' für HTTP 'POST' Requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
+ *
+ * Requests enthalten im Body die Felder des 'filter-form' Formulars.
+ * (http://expressjs.com/de/4x/api.html#req.body)
+ *
+ * Als Response wird das ejs-Template mit Geo Tag Objekten gerendert.
+ * Die Objekte liegen in einem Standard Radius um die Koordinate (lat, lon).
+ * Falls 'term' vorhanden ist, wird nach Suchwort gefiltert.
+ */
+
+app.post('/discovery', function (req, res) {
+    // Creat list of GeoTags in a certain radius
+    var toRender = geoTagModul.searchByRadius(req.body.latitude, req.body.longitude, 5)
+    // Reduce list to GeoTags with a certain infix
+    if (req.body.discovery !== undefined) {
+        toRender = geoTagModul.searchByTerm(req.body.discovery, toRender)
+    }
+    res.render('gta', {
+        taglist: toRender,
+        lati: req.body.latitude,
+        longi: req.body.longitude
+    });
+});
+/**
+ * Setze Port und speichere in Express.
+ */
+
+var port = 3000;
+app.set('port', port);
+
+/**
+ * Erstelle HTTP Server
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Horche auf dem Port an allen Netzwerk-Interfaces
+ */
+
+server.listen(port);
---
+++ Aufgabe4/gta_v4/public/javascripts/geotagging.js
@@ -0,0 +1,178 @@
+/* Dieses Skript wird ausgeführt, wenn der Browser index.html lädt. */
+
+// Befehle werden sequenziell abgearbeitet ...
+
+/**
+ * "console.log" schreibt auf die Konsole des Browsers
+ * Das Konsolenfenster muss im Browser explizit geöffnet werden.
+ */
+console.log("The script is going to start...");
+
+// Es folgen einige Deklarationen, die aber noch nicht ausgeführt werden ...
+
+// Hier wird die verwendete API für Geolocations gewählt
+// Die folgende Deklaration ist ein 'Mockup', das immer funktioniert und eine fixe Position liefert.
+GEOLOCATIONAPI = {
+    getCurrentPosition: function(onsuccess) {
+        onsuccess({
+            "coords": {
+                "latitude": 49.013790,
+                "longitude": 8.390071,
+                "altitude": null,
+                "accuracy": 39,
+                "altitudeAccuracy": null,
+                "heading": null,
+                "speed": null
+            },
+            "timestamp": 1540282332239
+        });
+    }
+};
+
+// Die echte API ist diese.
+// Falls es damit Probleme gibt, kommentieren Sie die Zeile aus.
+GEOLOCATIONAPI = navigator.geolocation;
+
+/**
+ * GeoTagApp Locator Modul
+ */
+var gtaLocator = (function GtaLocator(geoLocationApi) {
+
+    // Private Member
+
+    /**
+     * Funktion spricht Geolocation API an.
+     * Bei Erfolg Callback 'onsuccess' mit Position.
+     * Bei Fehler Callback 'onerror' mit Meldung.
+     * Callback Funktionen als Parameter übergeben.
+     */
+    var tryLocate = function(onsuccess, onerror) {
+        if (geoLocationApi) {
+            geoLocationApi.getCurrentPosition(onsuccess, function(error) {
+                var msg;
+                switch (error.code) {
+                    case error.PERMISSION_DENIED:
+                        msg = "User denied the request for Geolocation.";
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        msg = "Location information is unavailable.";
+                        break;
+                    case error.TIMEOUT:
+                        msg = "The request to get user location timed out.";
+                        break;
+                    case error.UNKNOWN_ERROR:
+                        msg = "An unknown error occurred.";
+                        break;
+                }
+                onerror(msg);
+            });
+        } else {
+            onerror("Geolocation is not supported by this browser.");
+        }
+    };
+
+    // Auslesen Breitengrad aus der Position
+    var getLatitude = function(position) {
+        return position.coords.latitude;
+    };
+
+    // Auslesen Längengrad aus Position
+    var getLongitude = function(position) {
+        return position.coords.longitude;
+    };
+
+    // Read the taglist saved in the image data
+    var getTagList = function() {
+        return JSON.parse(document.getElementById("result-img").dataset.tags)
+    };
+
+    var errorfunction = function(msg){
+        alert(msg);
+    };
+
+    var successfunction = function(position){
+
+        var latitude = getLatitude(position);
+        var longitude = getLongitude(position);
+        
+        document.getElementById("tagging_latitude_input").value = latitude;
+        document.getElementById("tagging_longitude_input").value = longitude;
+
+        document.getElementById("discovery_tagging_latitude_input").value = latitude;
+        document.getElementById("discovery_tagging_longitude_input").value = longitude;
+
+        var returnurl = getLocationMapSrc(latitude, longitude);
+        document.getElementById("result-img").src = returnurl;
+    };
+
+    // Hier Google Maps API Key eintragen
+    var apiKey = "FZ5UfGbai3sGj21u6ZJJkGLk351DCGa4";
+
+    /**
+     * Funktion erzeugt eine URL, die auf die Karte verweist.
+     * Falls die Karte geladen werden soll, muss oben ein API Key angegeben
+     * sein.
+     *
+     * lat, lon : aktuelle Koordinaten (hier zentriert die Karte)
+     * tags : Array mit Geotag Objekten, das auch leer bleiben kann
+     * zoom: Zoomfaktor der Karte
+     */
+    var getLocationMapSrc = function(lat, lon, tags, zoom) {
+        zoom = typeof zoom !== 'undefined' ? zoom : 15;
+
+        if (apiKey === "YOUR_API_KEY_HERE") {
+            console.log("No API key provided.");
+            return "images/mapview.jpg";
+        }
+
+        var tagList = "&pois=You," + lat + "," + lon;
+        if (tags !== undefined) tags.forEach(function(tag) {
+            tagList += "|" + tag.name + "," + tag.latitude + "," + tag.longitude;
+        });
+
+        var urlString = "https://www.mapquestapi.com/staticmap/v4/getmap?key=" +
+            apiKey + "&size=600,400&zoom=" + zoom + "&center=" + lat + "," + lon + "&" + tagList;
+
+        console.log("Generated Maps Url: " + urlString);
+        return urlString;
+    };
+
+    return { // Start öffentlicher Teil des Moduls ...
+
+        // Public Member
+
+        readme: "Dieses Objekt enthält 'öffentliche' Teile des Moduls.",
+
+        updateLocation: function() {
+            const latitudeElement = document.getElementById('tagging_latitude_input')
+            const longitudeElement = document.getElementById('tagging_longitude_input')
+            const hiddenLatitude = document.getElementById('discovery_tagging_latitude_input')
+            const hiddenLongitude = document.getElementById('discovery_tagging_longitude_input')
+            // If no values exist yet request location
+            if ((latitudeElement.value === "") ||
+                (longitudeElement.value === "") ||
+                (hiddenLatitude.value === "") ||
+                (hiddenLongitude.value === "") ||
+                (isNaN(latitudeElement.value)) ||
+                (isNaN(longitudeElement.value)) ||
+                (isNaN(hiddenLatitude.value)) ||
+                (isNaN(hiddenLongitude.value))) {
+                tryLocate(successfunction, errorfunction);
+            }
+            else {
+                var returnurl = getLocationMapSrc(hiddenLatitude.value, hiddenLongitude.value, getTagList(), 13);
+                document.getElementById("result-img").src = returnurl;
+            }
+        }
+
+    };
+})(GEOLOCATIONAPI);
+
+/**
+ * $(function(){...}) wartet, bis die Seite komplett geladen wurde. Dann wird die
+ * angegebene Funktion aufgerufen. An dieser Stelle beginnt die eigentliche Arbeit
+ * des Skripts.
+ */
+$(document).ready(
+    gtaLocator.updateLocation()
+);
