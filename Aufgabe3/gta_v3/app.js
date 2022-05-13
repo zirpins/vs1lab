918f30481e13b2ed2c47f17f6402fa00fea7a9c6
Michael Bangas <bami1022@h-ka.de>
2022-05-13 12:24:41 +0200

    added middleware for static files in app.js

--- Aufgabe3/gta_v3/app.js
+++ Aufgabe3/gta_v3/app.js
@@ -41,8 +41,7 @@ app.use(express.urlencoded({ extended: false }));
  * Configure path for static content.
  * Test the result in a browser here: 'http://localhost:3000/'.
  */
-
-// TODO: ... your code here ...
+app.use(express.static("public"));
 
 // Set dedicated script for routing
 app.use('/', indexRouter);
