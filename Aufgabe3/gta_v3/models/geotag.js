1a29b74fe12a02527765c4ef504ba124445d9bd6
Michael Bangas <bami1022@h-ka.de>
2022-05-13 09:53:34 +0200

    Added backend functions for geotags: class geotag.js, class geotag-store.js, class geotag-examples.js

--- Aufgabe3/gta_v3/models/geotag.js
+++ Aufgabe3/gta_v3/models/geotag.js
@@ -10,9 +10,49 @@
  * GeoTag objects should contain at least all fields of the tagging form.
  */
 class GeoTag {
+    #name = "";
+    #latitude = 0;
+    #longitude = 0;
+    #hashtag = "";
 
-    // TODO: ... your code here ...
-    
+    constructor(name, latitude, longitude, hashtag) {
+        this.#name = name;
+        this.#latitude = latitude;
+        this.#longitude = longitude;
+        this.#hashtag = hashtag;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    set name(name) {
+        this.#name = name;
+    }
+
+    get latitude() {
+        return this.#latitude;
+    }
+
+    set latitude(latitude) {
+        this.#latitude = latitude;
+    }
+
+    get longitude() {
+        return this.#longitude;
+    }
+
+    set longitude(longitude) {
+        this.#longitude = longitude;
+    }
+
+    get hashtag() {
+        return this.#hashtag;
+    }
+
+    set hashtag(hashtag) {
+        this.#hashtag = hashtag;
+    }
 }
 
 module.exports = GeoTag;
