ea4829ee333bfce5cfac018f872958f427becf8d
niroe22 <niki.roemer@gmail.com>
2022-05-23 16:38:45 +0200

    statische dateien

--- Aufgabe3/gta_v3/app.js
+++ Aufgabe3/gta_v3/app.js
@@ -43,6 +43,7 @@ app.use(express.urlencoded({ extended: false }));
  */
 
 // TODO: ... your code here ...
+app.use(express.static('public'));
 
 // Set dedicated script for routing
 app.use('/', indexRouter);
