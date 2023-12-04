8e7cc73019708620e0f03ea7980c45c26314771a
Jose Acena <joseracena@gmail.com>
2023-12-04 16:42:08 +0000

    ??? default

--- Aufgabe3/gta_v3/routes/index.js
+++ Aufgabe3/gta_v3/routes/index.js
@@ -30,6 +30,7 @@ const GeoTag = require('../models/geotag');
  */
 // eslint-disable-next-line no-unused-vars
 const GeoTagStore = require('../models/geotag-store');
+const{ route } = require('../app'); 
 
 /**
  * Route '/' for HTTP 'GET' requests.
