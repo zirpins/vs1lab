27411c3bc51165eb9705edff81618c750674637c
yenesbas <94112721+yenesbas@users.noreply.github.com>
2024-06-02 15:33:30 +0200

    Aufgabe3 vorbereitet

---
+++ Aufgabe3/gta_v3/public/javascripts/geotagging.js
@@ -0,0 +1,157 @@
+// File origin: VS1LAB A2
+
+/* eslint-disable no-unused-vars */
+
+// This script is executed when the browser loads index.html.
+
+// "console.log" writes to the browser's console. 
+// The console window must be opened explicitly in the browser.
+// Try to find this output in the browser...
+console.log("The geoTagging script is going to start...");
+
+/**
+  * A class to help using the HTML5 Geolocation API.
+  */
+class LocationHelper {
+    // Location values for latitude and longitude are private properties to protect them from changes.
+    #latitude = '';
+
+    /**
+     * Getter method allows read access to privat location property.
+     */
+    get latitude() {
+        return this.#latitude;
+    }
+
+    #longitude = '';
+
+    get longitude() {
+        return this.#longitude;
+    }
+
+   /**
+    * Create LocationHelper instance if coordinates are known.
+    * @param {string} latitude 
+    * @param {string} longitude 
+    */
+   constructor(latitude, longitude) {
+       this.#latitude = (parseFloat(latitude)).toFixed(5);
+       this.#longitude = (parseFloat(longitude)).toFixed(5);
+   }
+
+    /**
+     * The 'findLocation' method requests the current location details through the geolocation API.
+     * It is a static method that should be used to obtain an instance of LocationHelper.
+     * Throws an exception if the geolocation API is not available.
+     * @param {*} callback a function that will be called with a LocationHelper instance as parameter, that has the current location details
+     */
+    static findLocation(callback) {
+        const geoLocationApi = navigator.geolocation;
+
+        if (!geoLocationApi) {
+            throw new Error("The GeoLocation API is unavailable.");
+        }
+
+        // Call to the HTML5 geolocation API.
+        // Takes a first callback function as argument that is called in case of success.
+        // Second callback is optional for handling errors.
+        // These callbacks are given as arrow function expressions.
+        geoLocationApi.getCurrentPosition((location) => {
+            // Create and initialize LocationHelper object.
+            let helper = new LocationHelper(location.coords.latitude, location.coords.longitude);
+            // Pass the locationHelper object to the callback.
+            callback(helper);
+        }, (error) => {
+           alert(error.message)
+        });
+    }
+}
+
+/**
+ * A class to help using the Leaflet map service.
+ */
+class MapManager {
+
+    #map
+    #markers
+
+    /**
+    * Initialize a Leaflet map
+    * @param {number} latitude The map center latitude
+    * @param {number} longitude The map center longitude
+    * @param {number} zoom The map zoom, defaults to 18
+    */
+    initMap(latitude, longitude, zoom = 18) {
+        // set up dynamic Leaflet map
+        this.#map = L.map('map').setView([latitude, longitude], zoom);
+        var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+        L.tileLayer(
+            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; ' + mapLink + ' Contributors'}).addTo(this.#map);
+        this.#markers = L.layerGroup().addTo(this.#map);
+    }
+
+    /**
+    * Update the Markers of a Leaflet map
+    * @param {number} latitude The map center latitude
+    * @param {number} longitude The map center longitude
+    * @param {{latitude, longitude, name}[]} tags The map tags, defaults to just the current location
+    */
+    updateMarkers(latitude, longitude, tags = []) {
+        // delete all markers
+        this.#markers.clearLayers();
+        L.marker([latitude, longitude])
+            .bindPopup("Your Location")
+            .addTo(this.#markers);
+        for (const tag of tags) {
+            L.marker([tag.location.latitude,tag.location.longitude])
+                .bindPopup(tag.name)
+                .addTo(this.#markers);  
+        }
+    }
+}
+
+/**
+ * TODO: 'updateLocation'
+ * A function to retrieve the current location and update the page.
+ * It is called once the page has been fully loaded.
+ */
+// ... your code here ...
+
+function updateLocation() {
+    LocationHelper.findLocation((locationHelper) => {
+        const lat = locationHelper.latitude;
+        const lon = locationHelper.longitude;
+        
+        document.getElementById("tagging-lat").value = lat;
+        document.getElementById("tagging-lon").value = lon;
+
+        document.getElementById("tagging-lath").value = lat; // versteckte eingaben
+        document.getElementById("tagging-lonh").value = lon; 
+        //mapManager.initMap(lat, lon);
+        //mapManager.updateMarkers(lat, lon, []); 
+
+        //var mm = L.map('mapView', {center: [lat, lon],zoom: 13});
+        //L.marker([lat, lon]).addTo(mm);
+
+        const mapp = new MapManager;
+
+        mapp.initMap(lat,lon);
+        mapp.updateMarkers(lat,lon);
+        
+        //finder methode und remove
+        const mapViewImg = document.getElementById("mapView");
+        const resultSpan = document.getElementById('.discovery__map span');
+        if (mapViewImg) {
+            mapViewImg.remove();
+        }
+        if (resultSpan) {
+            resultSpan.remove();
+        }
+    });
+}
+
+// Wait for the page to fully load its DOM content, then call updateLocation
+document.addEventListener("DOMContentLoaded", () => {
+    updateLocation();
+});
