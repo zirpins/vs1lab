44857bf111e08acc05918bfc8e8626fe3766aab2
Jan <j.reisert@web.de>
2025-12-09 10:57:30 +0000

    created GeoTag class

--- Aufgabe3/gta_v3/models/geotag.js
+++ Aufgabe3/gta_v3/models/geotag.js
@@ -11,8 +11,44 @@
  */
 class GeoTag {
 
-    // TODO: ... your code here ...
-    
+    constructor(latitude, longitude, name, hashtag) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.name = name;
+        this.hashtag = hashtag;
+    }
+
+    getLatitude() {
+        return this.latitude;
+    }
+
+    setLatitude() {
+        this.latitude = this.latitude;
+    }
+
+    getLongitude() {
+        return this.longitude;
+    }
+
+    setLongitude() {
+        this.longitude = this.longitude;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    getHashtag() {
+        return this.hashtag;
+    }
+
+    setHashtag(hashtag) {
+        this.hashtag = hashtag;
+    }
 }
 
 module.exports = GeoTag;
